74261edb8e122f05de39d3b3c12cd1936c3a6f1f
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-08-17 09:47:54 +0600

    Update

--- src/components/hotel-edit/TagInput.jsx
+++ src/components/hotel-edit/TagInput.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 export default function TagInput({ data }) {
-  console.log(data);
+  const [active, setActive] = useState([data[0].name, data[3].name]);
+
   return (
     <ul className="tag-input">
       {data.map((d, i) => (
-        <li key={i}>
+        <li
+          onClick={() => {
+            if (!active.includes(d.name)) {
+              setActive((prev) => {
+                return [...prev, d.name];
+              });
+            } else {
+              const updatedItems = active.filter((item) => item !== d.name);
+              setActive(updatedItems);
+            }
+          }}
+          className={(active.includes(d.name) && "active") || ""}
+          key={i}
+        >
           {d.icon}
           {d.name}
         </li>
