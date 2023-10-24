f4ff59731bf45a4668bc796c9bbbcd0cfb8dae8f
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-10-24 22:42:26 +0600

    update

--- src/components/basic/Rating.jsx
+++ src/components/basic/Rating.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { AiFillStar } from "react-icons/ai";
 
-export default function Rating() {
+export default function Rating({ rate }) {
   const [rating, setRating] = useState(5);
+  console.log(rate);
   return (
     <ul className="rating">
-      {Array.from({ length: rating }, (d, i) => (
+      {Array.from({ length: rate }, (d, i) => (
         <li key={i}>
           <AiFillStar />
         </li>
